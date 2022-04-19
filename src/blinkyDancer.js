e5e91b381a48827d3bf62d37d1b9a7c5786d84a5
Joy <parkerjn90@gmail.com>
2022-04-19 08:11:06 -0400

    save pseudoclassical refactored code

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,16 +1,20 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this);
-  this.top = top;
-  this.left = left;
-  this.timeBetweenSteps = timeBetweenSteps;
+  Dancer.call(this, top, left, timeBetweenSteps);
+  // this.top = top;
+  // this.left = left;
+  // this.timeBetweenSteps = timeBetweenSteps;
+  // this.callCount = 0;
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function() {
-  Dancer.prototype.step(this.timeBetweenSteps);
+  // console.log('dancer.p.step: ', Dancer.prototype.step);
+  // console.log(this);
+  Dancer.prototype.step.call(this);
+
+  // this.callCount++;
+  // let callCount = this.callCount;
   this.$node.toggle();
 };
-
-
